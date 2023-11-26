afee00cfb88958bef01c588a44e5a2c65f5ba143
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-26 18:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  // Get elements
+  const searchComponent = document.getElementById('searchComponent');
+  const searchInput = document.getElementById('searchInput');
+  const searchButton = document.getElementById('searchButton');
+
+  // Function to toggle active state and focus input
+  function activateSearch() {
+    searchComponent.classList.add('active');
+    searchInput.focus();
+  }
+
+  // Event listener for the button click
+  searchButton.addEventListener('click', activateSearch);
+});
